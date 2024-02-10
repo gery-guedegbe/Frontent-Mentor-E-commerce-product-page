6dbe82596939771a60b3490cb6a3d5f888cb932b
Gery <you@example.com>
2024-02-10 20:22:13 +0100

    First commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div class="attribution" className="text-center p-5 text-sm md:text-base">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-orange-400 font-bold mx-1 "
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      Coded by{" "}
+      <a href="#" className="text-orange-400 font-bold mx-1 ">
+        Géry GUEDEGBE
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Footer;
